2c20e01124dab907ba0159786fc8c2311a0df1c9
Efren Ospino <efrenospinojr@gmail.com>
2020-07-31 21:44:30 -0500

    Add call to /api/users/login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const handleClick = () => {
+        const user = {username, password};
+        const url = 'http://localhost:80/api/users/login';
+        fetch(url, {
+            method: 'POST', 
+            body: JSON.stringify(user),
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then(res => res.json())
+        .then(json => alert(`User ${json.name} was authenticated.`))
+        .catch(_ => alert('Error trying to authenticate.'));
+    };
     return (
         <div>
             <form>
-                <p><label>Username</label><br/><input type="text" name="username"></input></p>
-                <p><label>Password</label><br/><input type="password" name="password"></input></p>
-                <p><button type="button">Login</button></p>
+                <p><label>Username</label><br/>
+                    <input 
+                        type="text" 
+                        name="username" 
+                        value={username} 
+                        onChange={(e) => {setUsername(e.target.value)}}/></p>
+                <p><label>Password</label><br/>
+                    <input 
+                        type="password" 
+                        name="password"
+                        value={password}
+                        onChange={(e) => {setPassword(e.target.value)}}/></p>
+                <p>
+                    <button 
+                        onClick={handleClick}
+                        type="button">Login</button></p>
             </form>
         </div>
     )
