fe323f83b229101e5a8a8c08470d242873257a1d
Efren Ospino <efrenospinojr@gmail.com>
2020-08-01 11:08:05 -0500

    Add new tweet

--- src/components/Private/Tweets/index.js
+++ src/components/Private/Tweets/index.js
@@ -2,32 +2,59 @@ import React, {useState, useEffect} from 'react';
 
 const Tweets = () => {
     const [tweets, setTweets] = useState();
+
     const getTweets = () => {
         const host = process.env.REACT_APP_API_URL;
         const url = `${host}/tweets`;
         fetch(url)
             .then(res => res.json())
             .then(json => {
-                console.log(json);
                 setTweets(json)
             });
     }
+
+    const [newTweet, setTweet] = useState('');
+    const handleClick = () => {
+        const host = process.env.REACT_APP_API_URL;
+        const url = `${host}/tweets`;
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({"content": newTweet}),
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': localStorage.getItem('jwt')
+            }
+        })
+        .then(_ => {
+            setTweet('')
+            getTweets();
+        })
+        .catch(_ => alert('Error creating tweet'));
+    };
+    
     useEffect(() => {
         getTweets()
     }, [])
+
     return <div>
+        <form>
+            <textarea 
+                placeholder='What are you thinking on?'
+                onChange={(e) => {setTweet(e.target.value)}}
+                value={newTweet}/>
+            <p><button 
+                onClick={handleClick}
+                type="button">New tweet</button></p>
+        </form>
         {
-            tweets ? 
-                tweets.map(t => 
-                    <div key={t._id}>
-                        <br/>
+            tweets && tweets.map(t => 
+                <div key={t._id}>
                         <p>{t.content}</p>
                         <p>@{t.user.username}</p>
                         <p>Comments: {t.comments.length}</p>
                         <p>{t.createdAt}</p>
+                        <hr/>
                 </div>)
-                : 
-                <p>There's nothing to show</p>
         }
     </div>
 }
